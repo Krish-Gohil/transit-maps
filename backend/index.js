f15f90f224c76e7ce10efddb573f41fb495c6f1d
Krish Gohil <krishgohil843@gmail.com>
2025-09-06 00:22:50 -0400

    added health check route

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,14 @@ import cors from 'cors'
 import nearbyStops from "./routes/nearbyStops.js";
 import etaDataController from "./routes/etaDataController.js";
 import routingStopsList from "./routes/routingStopsList.js";
-
+import healthCheck from "./routes/healthCheck.js";
 dotenv.config()
 const app = express()
 app.use(cors())
 
 app.use('/api/stops', nearbyStops)
 app.use('/api/eta', etaDataController)
-// app.use('/api/routingStopsList', routingStopsList)
+app.use('/api/health', healthCheck)
 
 const port = process.env.PORT || 3000
 
